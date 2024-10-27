9f1100bd4eff4b8ddf65b320faac0bd9fe01d466
charan028 <67135079+charan028@users.noreply.github.com>
2024-10-27 11:58:11 -0500

    auth changes

--- server/routes/ingredientRoutes.js
+++ server/routes/ingredientRoutes.js
@@ -6,6 +6,10 @@ const Ingredient = require("../models/ingredient"); // Ensure the path is correc
 // Route to render the ingredients page with data
 router.get("/ingredients", async (req, res) => {
   try {
+    if (!req.session.userId || !req.session.username) {
+      console.error("Error: Missing userId or username in session.");
+      return res.status(401).json({ error: "Unauthorized. Please log in." });
+    }
     const ingredients = await Ingredient.find({}); // Fetch all ingredients from MongoDB
     res.render("ingredients", { ingredients }); // Render ingredients.ejs and pass ingredients data
   } catch (error) {
